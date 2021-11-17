692870e921ebbb891e1546783bf808058aea0422
Crowe Jacobs <croweisme.jpg@gmail.com>
2021-11-17 12:52:57 -0600

    First commit

---
+++ 2-3.PersonalMesagePROJ.py
@@ -0,0 +1,5 @@
+#Use a variable to represent a person name, and print a message to that person 
+
+name = input("Hey, What's your name? \n")
+
+print(f"Nice to meet you, {name.title()}.")
---
+++ 2-4.NameCasesPROJ.py
@@ -0,0 +1,6 @@
+#Use a variable to represent a persons name and then print that name in lowercase, uppercase, and title case.
+
+name = input("Hey, What's your name? \n")
+print(f"Title Case: {name.title()}")
+print(f"Lower Case: {name.lower()}")
+print(f"Upper Case: {name.upper()}")
